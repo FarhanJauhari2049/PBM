86783f2e3ad3a9a8b28f4cbba36139526f1b9829
YusniaDewi <72717399+YusniaDewi@users.noreply.github.com>
2021-11-02 21:56:18 +0700

    hapus

--- lib/detail/body.dart
+++ lib/detail/body.dart
@@ -11,16 +11,6 @@ class Body extends StatelessWidget {
     return Column(
         
         children: <Widget>[
-          //   Padding(
-          //     padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
-          //     child: Text(
-          //       "Flower Shop",
-          //       style: Theme.of(context)
-          //           .textTheme
-          //           .headline5
-          //           .copyWith(fontWeight: FontWeight.bold),
-          //     ),
-          //   ),
           Expanded(
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -11,7 +11,6 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: buildAppBar(),
       body: Body(),
-      // bottomNavigationBar: LayoutNavigationBar(),
     );
   }
 
