e2a39c7a7a1bc5bffdfb492363ab284d28b0c925
YusniaDewi <72717399+YusniaDewi@users.noreply.github.com>
2021-11-07 08:23:48 +0700

    commit

--- lib/detail/bouquet.dart
+++ lib/detail/bouquet.dart
@@ -1,8 +1,10 @@
 import 'package:flower_shop/constants.dart';
-import 'package:flower_shop/detail/body.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:flower_shop/screens/home/navbar.dart';
+import 'item_card.dart';
+import 'package:flower_shop/screens/order/details.dart';
+import 'package:flower_shop/detail/productBouquet.dart';
 
 class Bouquet extends StatelessWidget {
   @override
@@ -42,5 +44,33 @@ class Bouquet extends StatelessWidget {
         SizedBox(width: kDefaultPaddin / 2)
       ],
     );
+  }
+}
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
+              child: GridView.builder(
+                  itemCount: products.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: kDefaultPaddin,
+                    crossAxisSpacing: kDefaultPaddin,
+                    childAspectRatio: 0.75,
+                  ),
+                  itemBuilder: (context, index) => ItemCard(
+                        product: products[index],
+                        press: () => Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => Details())),
+                      )),
+            ),
+          )
+        ]);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Shop',
       theme: ThemeData(
         textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
         visualDensity: VisualDensity.adaptivePlatformDensity,
