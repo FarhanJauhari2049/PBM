d0cfd3473b84e5f180288c9ae47725312d7af7b3
FarhanJauhari2049 <farhanutami25@gmail.com>
2021-12-20 20:57:16 +0700

    update

--- lib/detail2/order.dart
+++ lib/detail2/order.dart
@@ -5,21 +5,40 @@ class AddToOrder extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(context),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          return Card(
-              child: ListTile(
-                  title:
-                      Text("Baby Breath Bouquet", style: TextStyle(fontSize: 30)),
+      body: ListView(
+        children: [
+          ListTile(
+            title:Text("Baby Breath Bouquet", style: TextStyle(fontSize: 30)),
                   subtitle: Text("anda telah membeli barang ini"),
                   leading: CircleAvatar(
-                    backgroundImage: AssetImage("assets/images/gambar2.jpeg",),
-                  )
-                  ));
-        },
-      ),
+                  backgroundImage: AssetImage("assets/images/gambar2.jpeg",),
+          )),
+          ListTile(
+            title:Text("Wedding Bouquet", style: TextStyle(fontSize: 30)),
+                  subtitle: Text("anda telah membeli barang ini"),
+                  leading: CircleAvatar(
+                  backgroundImage: AssetImage("assets/images/gambar9.jpeg",),
+          )),
+        ],
+      )
+      // ListView.builder(
+      //   itemBuilder: (context, index) {
+      //     return Card(
+      //         child: ListTile(
+      //             title:
+      //                 Text("Baby Breath Bouquet", style: TextStyle(fontSize: 30)),
+      //             subtitle: Text("anda telah membeli barang ini"),
+      //             leading: CircleAvatar(
+      //               backgroundImage: AssetImage("assets/images/gambar2.jpeg",),
+      //             )
+      //             )
+                  
+      //             );
+      //   },
+      // ), 
     );
   }
+  
 
   AppBar buildAppBar(BuildContext context) {
     return AppBar(
